90c54edfcd293658795629a96035bccfe2af7239
codenoob5311 <ae9879026@gmail.com>
2025-08-28 12:09:18 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ st.set_page_config(
 
 st.header("Welcome To D & A 👋")
 st.write("Destination :")
-st.write("1- Simplift Data") # تبسيط البيانات
-st.write("2- Make Graphics") # عمل رسومات بيانيه
-st.write("3- Reaching a Decision") # الوصول الي قرار
+st.write(" Simplift Data") # تبسيط البيانات
+st.write(" Make Graphics") # عمل رسومات بيانيه
+st.write(" Reaching a Decision") # الوصول الي قرار
 
 st.sidebar.success("All Bages.")
